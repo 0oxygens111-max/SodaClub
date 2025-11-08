a47e0e4e2ed788cf36f6f0fc25954bbebcf0a751
0oyxgenzGanteng <0oxygens111@gmail.com>
2025-11-08 23:09:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Loading Screen
+window.addEventListener("load", () => {
+  const loadingScreen = document.getElementById("loading-screen");
+  setTimeout(() => {
+    loadingScreen.classList.add("hidden");
+  }, 1500); // 1.5 detik sebelum hilang
+});
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
+// Smooth scroll untuk semua link navbar
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault(); // cegah default jump
+    const target = document.querySelector(this.getAttribute('href'));
+    const offset = 70; // tinggi navbar
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const targetRect = target.getBoundingClientRect().top;
+    const targetPosition = targetRect - bodyRect - offset;
+
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth'
+    });
+  });
+});
